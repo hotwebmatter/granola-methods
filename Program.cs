2c2098cf7fab344be3f09b8d037df16d609ad55d
Matt Obert on Windows <matt@hotwebmatter.com>
2019-12-22 12:15:13 -0500

    Initial commit of granola-methods project scaffold

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace granola_methods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
