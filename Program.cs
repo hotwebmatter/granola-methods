d0caa225cf28b1397f241f8f73c43f48152bd77c
Matt Obert on Windows <matt@hotwebmatter.com>
2019-12-22 12:17:23 -0500

    Use static System.Console

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using static System.Console;
 
 namespace granola_methods
 {
@@ -6,7 +7,7 @@ namespace granola_methods
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            WriteLine("Hello World!");
         }
     }
 }
