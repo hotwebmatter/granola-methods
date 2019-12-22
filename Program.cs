4a92afd34853b96e14e0029751bf08bf59d6b5a6
Matt Obert on Windows <matt@hotwebmatter.com>
2019-12-22 12:30:18 -0500

    Declare constants and variables

--- Program.cs
+++ Program.cs
@@ -5,6 +5,18 @@ namespace granola_methods
 {
     class Program
     {
+        // class-level constants
+        const double BARS_PER_CASE = 12,
+                     COST_PER_CASE = 5,
+                     STUDENT_GOVT_PERCENTAGE = .1;
+        // class-level static variables
+        static double casesSold,
+                      pricePerBar,
+                      grossReceipts,
+                      wholesaleCost,
+                      netProfits,
+                      studentGovtFee,
+                      profitAfterFee;
         static void Main(string[] args)
         {
             WriteLine("Hello World!");
